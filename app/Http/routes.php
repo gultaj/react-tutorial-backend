4559c4ab28dc9c9f6dc5454ec62e38a2d4de897f
gultaj <yaschick.mail@gmail.com>
2016-03-18 01:18:21 +0300

    login/logout

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 $app->get('/', function () use ($app) {
     return $app->version();
@@ -26,15 +27,23 @@ $app->get('/users', function(Request $request) {
 });
 
 $app->post('/auth/login', function(Request $request) {
-	return response(Auth::check());
-	$a = Auth::attempt([
-		'email' => 'Linda12@Hermann.com',
-		'password' => 'secret',
-		'active' => '1'
-	]);
-	return  $a;
-	// 	return response(['success']);
-	// 	// return response(Auth::user())->header('Access-Control-Allow-Origin', '*');
+	if ($user = App\User::where('email', $request->input('email'))->first()) {
+		if (Hash::check($request->input('password'), $user->password)) {
+			$user->remember_token = bin2hex(random_bytes(50));
+			$user->save();
+			$user = collect($user)->only('id', 'nickname', 'remember_token');
+			return response($user)->header('Access-Control-Allow-Origin', '*');
+		}
+	}
+	return null;
+});
 
-	// return null;
+$app->post('/auth/logout', function(Request $request) {
+	if ($user = App\User::where('remember_token', $request->input('remember_token'))->first()) {
+		$user->remember_token = null;
+		$user->save();
+		return response(['logout'])->header('Access-Control-Allow-Origin', '*');
+	}
+	return response([$request->input('remember_token')])->header('Access-Control-Allow-Origin', '*');
+	return null;
 });
